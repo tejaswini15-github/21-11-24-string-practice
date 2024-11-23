c31810cbeb5038f0de12f5164ff7cf32d56e264c
tejaswini15-github <pendotatejaswini2000@gmail.com>
2024-11-24 03:48:42 +0530

    string practice

---
+++ index.js
@@ -0,0 +1,64 @@
+var str1 ='Hello World'
+console.log(str1);
+var str2 ="Hello World"
+console.log(str2);
+var str3 =`Hello World`
+console.log(str3);
+var str4 =new String("Hello World")
+console.log(str4);
+
+var str5="Hello";
+var str6="There";
+console.log(str5+str6);
+var str7=str5 +str6;
+console.log(str7);
+
+var str5 ="Hello"
+var str6 ="There"
+var str7=str5+' '+str6;
+console.log(str7);
+
+var str8 ="Teja"
+var str9 ="Chicken"
+var str10="Thumbsup"
+var str11=str8+" has ate "+str9+" with combination of "+ str10
+console.log(str11);
+
+
+var str12='I am ' + str8 + " from hyderabad, i stay in hostel and on sundays in my hostel "+ str9 + " is available and i purchase "+ str10 +" for cooldrink";
+console.log(str12);
+
+// var str13='I am ' ${str8} " from hyderabad, i stay in hostel and on sundays in my hostel "${str9} " is available and i purchase " ${str10}" for cooldrink";
+// console.log(str13);
+
+var a=10;
+var b=20;
+var c =a+b;
+console.log("The sum of a+b is",c);
+console.log("The sum of a+b is",a+b);
+console.log("The sum of a+b is"+c);
+console.log("The sum of a+b is"+(a+b));
+// console.log("The sum of a+b is"${a+b});
+
+
+console.log("2+2");
+console.log(2+"2");
+console.log("100"-2);
+console.log("2"+4);
+console.log("2"+true);
+console.log("false"+true);
+console.log(2/"2");
+console.log("2"+2+"10"+(100+30));
+console.log("10 "+10);
+console.log("10"+100+ +"100");
+console.log("2"*2);
+console.log("10"/2);
+console.log(10+ +"2" +100);
+console.log(10+ +"100"*2);
+console.log(typeof +"100");
+console.log("");
+
+
+
+
+
